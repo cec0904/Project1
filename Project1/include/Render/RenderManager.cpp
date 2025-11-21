05bbfa0dd31a575a0160e1f1871bb10d6cc6c7f8
cec0904 <cec0904@naver.com>
2025-11-21 10:39:22 +0900

    완성

--- Project1/include/Render/RenderManager.cpp
+++ Project1/include/Render/RenderManager.cpp
@@ -1,1 +1,29 @@
 #include "RenderManager.h"
+#include "RenderStateManager.h"
+
+CRenderManager::CRenderManager()
+{
+
+}
+
+CRenderManager::~CRenderManager()
+{
+	SAFE_DELETE(mStateManager);
+}
+
+bool CRenderManager::Init()
+{
+	mStateManager = new CRenderStateManager;
+
+	if (!mStateManager->Init())
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void CRenderManager::Render()
+{
+
+}
--- Project1/include/Render/RenderManager.h
+++ Project1/include/Render/RenderManager.h
@@ -1,5 +1,22 @@
 #pragma once
+
+#include "../GameInfo.h"
 class CRenderManager
 {
+	DECLARE_SINGLETONE(CRenderManager);
+
+private:
+	class CRenderStateManager* mStateManager = nullptr;
+
+public:
+	class CRenderStateManager* GetStateManager()
+	{
+		return mStateManager;
+	}
+
+public:
+	bool Init();
+	void Render();
+
 };
 
